f9ece951444b3a90b545d495d66f4a586c19fed3
Vatish <er.vatish@gmail.com>
2020-06-28 14:44:02 +0530

    Initial Commit

---
+++ DynamicHeightCV/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DynamicHeightCV
+//
+//  Created by Vatish Sharma on 28/06/20.
+//  Copyright © 2020 Hunt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
